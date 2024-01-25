6f7ed4b4daf25e20887b18081e5a0bb35c98e471
MeenakshiK16 <meenakshik.21eie@kongu.edu>
2024-01-25 16:10:39 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./Pages/Home"
+import Contact from "./Pages/Contact"
+import About from "./Pages/About"
+import Layout from "./Pages/Layout"
+import Service from "./Pages/Service"
+
+export default function App() {
+  return (
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="service" element={<Service/>}/>
+        </Route>
+      </Routes>
+      </BrowserRouter>
+  )
+}
+
