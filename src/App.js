a87aa293650a7e659861a034c65fc4b3025de478
MeenakshiK16 <meenakshik.21eie@kongu.edu>
2024-01-26 16:17:22 +0530

    version

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Contact from "./Pages/Contact"
 import About from "./Pages/About"
 import Layout from "./Pages/Layout"
 import Service from "./Pages/Service"
+import Counter from "./Pages/Counter"
+import Effect from "./Pages/Effect"
+import Reducer from "./Pages/Reducer"
+import Callbacks from "./Pages/Callbacks"
 
 export default function App() {
   return (
@@ -14,9 +18,12 @@ export default function App() {
           <Route path="about" element={<About/>}/>
           <Route path="contact" element={<Contact/>}/>
           <Route path="service" element={<Service/>}/>
+          <Route path="counter" element={<Counter/>}/>
+          <Route path="effect" element={<Effect/>}/>
+          <Route path="reducer" element={<Reducer/>}/>
+          <Route path="callback" element={<Callbacks/>}/>
         </Route>
       </Routes>
       </BrowserRouter>
   )
 }
-
--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -4,12 +4,17 @@ import { Outlet,Link} from "react-router-dom";
 export default function Layout(){
     return(
         <div>
+
             <nav>
                 <ul className='nav-bar'>
                     <li className='nav-link'><Link to="/">Home</Link></li>
                     <li className='nav-link'><Link to="/about">About</Link></li>
                     <li className='nav-link'><Link to="/contact">Contact</Link></li>
                     <li className='nav-link'><Link to="/services">Services</Link></li>
+                    <li className='nav-link'><Link to="/counter">Counter</Link></li>
+                    <li className='nav-link'><Link to="/effect">Effect</Link></li>
+                    <li className='nav-link'><Link to="/reducer">Reducer</Link></li>
+                    <li className='nav-link'><Link to="/callback">Callbacks</Link></li>
                 </ul>
             </nav>
             <Outlet/>
